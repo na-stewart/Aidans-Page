3cfa9c6d403a3b9b4b493b79757f346bdb7ab473
Aidan Stewart <na.stewart365@gmail.com>
2023-04-10 11:45:03 -0400

    Datatable implementation

--- blog/static/dashboard/js/main.js
+++ blog/static/dashboard/js/main.js
@@ -1,3 +1,16 @@
-$(document).ready( function () {
-  $('#myTable').DataTable();
-} );
+$(document).ready(function () {
+  var table = $('#example').DataTable();
+
+  $('#example tbody').on('click', 'tr', function () {
+      if ($(this).hasClass('selected')) {
+          $(this).removeClass('selected');
+      } else {
+          table.$('tr.selected').removeClass('selected');
+          $(this).addClass('selected');
+      }
+  });
+
+  $('#button').click(function () {
+      table.row('.selected').remove().draw(false);
+  });
+});
