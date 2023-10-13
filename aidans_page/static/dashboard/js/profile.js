52d1bf1c0de18bbfa3fcd61362fb8e28dc186ac0
na-stewart <na.stewart365@gmail.com>
2023-10-13 10:43:56 -0400

    Profile implementation

---
+++ aidans_page/static/dashboard/js/profile.js
@@ -0,0 +1,79 @@
+
+function init(){
+  addProfileFormEventListener();
+  fetch(`/api/v1/account`, {
+    method: 'GET',
+  })
+  .then(response => {
+    if (response.ok) 
+      return response.json();
+    return Promise.reject(response); 
+  })
+  .then(json => {
+    document.getElementById("email").value = json.data.email;
+    document.getElementById("username").value = json.data.username;
+  })
+  .catch(error => {
+    error.json().then(error => {
+      if (isAuthTokenInvalid(error) && window.location.pathname == '/profile')
+        location.assign("/login?redirect=" + window.location.pathname);
+    });
+  });
+}
+
+function addProfileFormEventListener(){
+  document.getElementById('profile-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    switch(event.submitter.value) {
+      case 'Update':
+        fetch('/api/v1/account/my', {
+          method: 'PUT',
+          body: new FormData(this)
+        })
+        .then(response => {
+          if (response.ok) 
+            return response.json();
+          return Promise.reject(response); 
+        })
+        .then(json => {
+          document.getElementById("response-msg").innerHTML = json.message;
+        })
+        .catch(error => {
+          error.json().then(error => document.getElementById("response-msg").innerHTML = error.message);
+        });
+      break;
+      case 'Delete Account':
+        fetch(`/api/v1/account/my`, {
+          method: 'DELETE',
+        })
+        .then(response => {
+          if (response.ok) 
+            return response.json();
+          return Promise.reject(response); 
+        })
+        .then(json => {
+          location.assign("/");
+        })
+        .catch(error => {
+          error.json().then(error => document.getElementById("response-msg").innerHTML = error.message);
+        });
+      break;
+      default:
+        fetch(`/api/v1/logout`, {
+          method: 'POST',
+        })
+        .then(response => {
+          if (response.ok) 
+            return response.json();
+          return Promise.reject(response); 
+        })
+        .then(json => {
+          location.assign("/login?redirect=" + window.location.pathname);
+        })
+        .catch(error => {
+          error.json().then(error => document.getElementById("response-msg").innerHTML = error.message);
+        }); 
+    }  
+  });
+}
+
